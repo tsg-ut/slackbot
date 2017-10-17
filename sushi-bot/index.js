a344ba57a66ef784ce04a524e209b84e2a8d9ba3
cookie-s <cookie-s@users.noreply.github.com>
2017-10-17 21:19:07 +0900

    akouryy func

--- sushi-bot/index.js
+++ sushi-bot/index.js
@@ -9,13 +9,28 @@ module.exports = (clients) => {
             return;
         }
 
-        const rtext = text.
+        let rtext = text;
+        rtext = rtext.
             replace(/鮨/g, 'すし').
             replace(/(su|ス|ズ|ず|寿|壽)/g, 'す').
             replace(/(si|shi|ci|し|シ|司)/g, 'し');
+        rtext = rtext.
+            replace(/akouryyy/g, 'akkoury').
+            replace(/akouryy/g, '').
+            replace(/kk/g, 'k').
+            replace(/rr/g, 'r').
+            replace(/y/g, 'yy');
 
         if (rtext.includes("すし")) {
             slack.reactions.add('sushi', {channel, timestamp});
+        }
+        if (rtext.includes("殺") || rtext.includes("死")) {
+            slack.reactions.add('no_good', {channel, timestamp});
+            slack.reactions.add('shaved_ice', {channel, timestamp});
+        }
+        if (rtext.includes("akouryy")) {
+            slack.reactions.add('no_good', {channel, timestamp});
+            slack.reactions.add('akouryy', {channel, timestamp});
         }
     });
 };
