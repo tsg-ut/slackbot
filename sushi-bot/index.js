8a49ed534352432cfb8843c0bac63d04b2f48c79
cookie-s <cookie-s@users.noreply.github.com>
2017-10-17 20:49:58 +0900

    more various sushi

--- sushi-bot/index.js
+++ sushi-bot/index.js
@@ -4,15 +4,16 @@ const emoji = require("node-emoji");
 const { RTM_EVENTS } = require("@slack/client")
 
 module.exports = (clients) => {
-	const { rtmClient: rtm, webClient: slack } = clients;
+    const { rtmClient: rtm, webClient: slack } = clients;
 
-	rtm.on(RTM_EVENTS.MESSAGE, async (message) => {
-		if (message.subtype) {
-			return;
-		}
-		const { channel, text, user, ts: timestamp } = message;
-        if (text.includes("すし") || text.includes("寿司") || text.includes("スシ")) {
+    rtm.on(RTM_EVENTS.MESSAGE, async (message) => {
+        const { channel, text, user, ts: timestamp } = message;
+        const rtext = text.
+            replace(/鮨/g, 'すし').
+            replace(/(su|ス|ズ|ず|寿|壽)/g, 'す').
+            replace(/(si|shi|ci|し|シ|司)/g, 'し');
+        if (rtext.includes("すし")) {
             slack.reactions.add('sushi', {channel, timestamp});
         }
-	});
+    });
 };
