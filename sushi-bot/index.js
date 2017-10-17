1de5e88d09a23fd041e28240f691de229232cb6e
cookie-s <cookie-s@users.noreply.github.com>
2017-10-17 20:57:34 +0900

    no text

--- sushi-bot/index.js
+++ sushi-bot/index.js
@@ -8,10 +8,15 @@ module.exports = (clients) => {
 
     rtm.on(RTM_EVENTS.MESSAGE, async (message) => {
         const { channel, text, user, ts: timestamp } = message;
+        if (!text) {
+            return;
+        }
+
         const rtext = text.
             replace(/鮨/g, 'すし').
             replace(/(su|ス|ズ|ず|寿|壽)/g, 'す').
             replace(/(si|shi|ci|し|シ|司)/g, 'し');
+
         if (rtext.includes("すし")) {
             slack.reactions.add('sushi', {channel, timestamp});
         }
