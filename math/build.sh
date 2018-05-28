f41db688c46d60435c8934b14d188cfb44351df9
Koki Takahashi <hakatasiloving@gmail.com>
2018-05-28 22:58:22 +0900

    math: Detect syntax error and runtime error before execution

--- math/build.sh
+++ math/build.sh
@@ -1,10 +1,24 @@
+#!/usr/bin/env bash
+
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 <equation>" >&2
   exit 1
 fi
 
 input=$(echo "$1" | tr -d "\n ;")
-result=$(maxima --batch-string="display2d:false;$input;" --very-quiet -s | tail -n +5 | tr -d "\n ")
+output=$(maxima --batch-string="display2d:false;errormsg:false;errcatch($input);" --very-quiet -s | tail -n +6)
+
+if [[ "$output" == incorrect* ]]; then
+  echo "Syntax error" >&2
+  exit 1
+fi
+
+result=$(echo "$output" | tail -n +2 | tr -d "\n ")
+
+if [[ "$result" = "[]" ]]; then
+  echo "Runtime error" >&2
+  exit 1
+fi
 
 if [ $input = $result ]; then
   echo "Result is identical to the input" >&2
