5b4d0c805d4dd3068bc17f88396c82109b3fbd68
Koki Takahashi <hakatasiloving@gmail.com>
2018-06-04 15:45:09 +0900

    mahjong: Add initial test

---
+++ mahjong/index.test.js
@@ -0,0 +1,20 @@
+/* eslint-env node, jest */
+
+const mahjong = require('./index.js');
+const Slack = require('../lib/slackMock.js');
+
+let slack = null;
+
+beforeEach(() => {
+	slack = new Slack();
+	process.env.CHANNEL_SANDBOX = slack.fakeChannel;
+	mahjong(slack);
+});
+
+describe('mahjong', () => {
+	it('responds to "配牌"', async () => {
+		const {username} = await slack.getResponseTo('配牌');
+
+		expect(username).toBe('mahjong');
+	});
+});
