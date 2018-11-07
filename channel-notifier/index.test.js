267c045d65ea257de44ca706a67c71e9ccab60ca
Koki Takahashi <hakatasiloving@gmail.com>
2018-11-07 15:23:48 +0900

    Fix test of channel-notifier

--- channel-notifier/index.test.js
+++ channel-notifier/index.test.js
@@ -1,5 +1,7 @@
 /* eslint-env node, jest */
 
+jest.mock('axios');
+
 const channelNotifier = require('./index.js');
 const Slack = require('../lib/slackMock.js');
 
@@ -12,24 +14,14 @@ beforeEach(() => {
 });
 
 it('responds to channel creation', () => new Promise((resolve) => {
-	Promise.all([
-		new Promise((resolve) => {
-			slack.on('channels.join', ({name}) => {
-				expect(name).toBe('#fun');
-				resolve();
-			});
-		}),
-		new Promise((resolve) => {
-			slack.on('chat.postMessage', ({channel, text, username, icon_emoji: icon}) => {
-				expect(channel).toBe(slack.fakeChannel);
-				expect(username).toBe('channel-notifier');
-				expect(text).toContain('fun');
-				expect(text).toContain('作成');
-				expect(icon).toBe(':new:');
-				resolve();
-			});
-		}),
-	]).then(resolve);
+	slack.on('chat.postMessage', ({channel, text, username, icon_emoji: icon}) => {
+		expect(channel).toBe(slack.fakeChannel);
+		expect(username).toBe('channel-notifier');
+		expect(text).toContain('fun');
+		expect(text).toContain('作成');
+		expect(icon).toBe(':new:');
+		resolve();
+	});
 
 	// https://api.slack.com/events/channel_created
 	slack.rtmClient.emit('channel_created', {
