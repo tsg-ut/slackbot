260167cd25eca5577cddf87dfee656da4da7749d
Koki Takahashi <hakatasiloving@gmail.com>
2018-11-07 14:46:11 +0900

    Implements channel-notifier

---
+++ channel-notifier/index.js
@@ -0,0 +1,18 @@
+const {stripIndent} = require('common-tags');
+
+module.exports = (clients) => {
+	const {rtmClient: rtm, webClient: slack} = clients;
+
+	rtm.on('channel_created', async (data) => {
+		await slack.channels.join(`#${data.channel.name}`);
+		await slack.chat.postMessage({
+			channel: process.env.CHANNEL_RANDOM,
+			text: stripIndent`
+				<@${data.channel.creator}> が <#${data.channel.id}|${data.channel.name}> を作成しました
+			`,
+			username: 'channel-notifier',
+			// eslint-disable-next-line camelcase
+			icon_emoji: ':new:',
+		});
+	});
+};
