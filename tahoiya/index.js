7bba8d5e1b81deae6acc69b965398c3544360b23
Koki Takahashi <hakatasiloving@gmail.com>
2019-01-24 12:26:22 +0900

    tahoiya: Update cache key of tahoiyabot-02

--- tahoiya/index.js
+++ tahoiya/index.js
@@ -746,11 +746,11 @@ module.exports = async ({rtmClient: rtm, webClient: slack}) => {
 					const body = text.replace(/^@\w+/, '').replace(/(って(なに|何)|とは)[？?⋯…・]*$/, '').trim();
 					const ruby = hiraganize(await getReading(body)).replace(/[^\p{Script=Hiragana}ー]/gu, '');
 					const modelData = text.startsWith('@tahoiya2') ? (
-						['tahoiyabot-02', 'model.ckpt-418000']
+						['tahoiyabot-02', 'model.ckpt-418000-ver2']
 					) : text.startsWith('@tahoiya') ? (
 						['tahoiyabot-01', 'model.ckpt-455758']
 					) : (
-						['tahoiyabot-02', 'model.ckpt-418000']
+						['tahoiyabot-02', 'model.ckpt-418000-ver2']
 					);
 
 					if (ruby.length > 0 && ruby.length <= 20) {
