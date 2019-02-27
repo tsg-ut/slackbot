de2b58e809c2911c58afa6a223cddcf4721888c7
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2019-02-27 22:52:39 +0900

    fix

--- index.js
+++ index.js
@@ -30,25 +30,25 @@ const rtmClient = new RTMClient(process.env.SLACK_TOKEN);
 const webClient = new WebClient(process.env.SLACK_TOKEN);
 (async () => {
 	await Promise.all(plugins.map((plugin) => plugin({rtmClient, webClient})));
+
+	logger.info('Launched');
+	webClient.chat.postMessage({
+		channel: process.env.CHANNEL_SANDBOX,
+		text: 'ｼｭｯｼｭｯ (起動音)',
+		username: 'slackbot',
+	});
+
+	let firstLogin = true;
+	rtmClient.on('authenticated', (data) => {
+		logger.info(`Logged in as ${data.self.name} of team ${data.team.name}`);
+		if (!firstLogin) {
+			webClient.chat.postMessage({
+				channel: process.env.CHANNEL_SANDBOX,
+				text: '再接続しました',
+				username: 'slackbot',
+			});
+		}
+		firstLogin = false;
+	});
+	rtmClient.start();
 })();
-logger.info('Launched');
-webClient.chat.postMessage({
-	channel: process.env.CHANNEL_SANDBOX,
-	text: 'ｼｭｯｼｭｯ (起動音)',
-	username: 'slackbot',
-});
-
-let firstLogin = true;
-rtmClient.on('authenticated', (data) => {
-	logger.info(`Logged in as ${data.self.name} of team ${data.team.name}`);
-	if (!firstLogin) {
-		webClient.chat.postMessage({
-			channel: process.env.CHANNEL_SANDBOX,
-			text: '再接続しました',
-			username: 'slackbot',
-		});
-	}
-	firstLogin = false;
-});
-rtmClient.start();
-
