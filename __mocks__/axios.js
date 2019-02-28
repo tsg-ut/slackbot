0fbc10016b7b20a19b00326039266ef38730db63
Koki Takahashi <hakatasiloving@gmail.com>
2019-02-28 12:26:25 +0900

    Make independent of request module

--- __mocks__/axios.js
+++ __mocks__/axios.js
@@ -1,6 +1,18 @@
 /* eslint-env node, jest */
 
-const axios = jest.fn((options) => Promise.resolve(axios.response));
+const {PassThrough} = require('stream');
+
+const axios = jest.fn((options = {}) => {
+	if (options.responseType === 'stream') {
+		const stream = new PassThrough();
+		process.nextTick(() => {
+			stream.end(axios.response);
+		});
+		return Promise.resolve({data: stream});
+	}
+
+	return Promise.resolve(axios.response);
+});
 axios.get = axios;
 axios.post = axios;
 
