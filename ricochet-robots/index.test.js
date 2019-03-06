d8d6608407926adeb3466f4b73ce82b14d356a3c
Koki Takahashi <hakatasiloving@gmail.com>
2019-03-06 10:36:50 +0900

    ricochet-robots: Extend timeout of some test cases

--- ricochet-robots/index.test.js
+++ ricochet-robots/index.test.js
@@ -12,27 +12,27 @@ describe('hyperrobot', () => {
 	beforeAll(() => {
 		slack = new Slack();
 		process.env.CHANNEL_SANDBOX = slack.fakeChannel;
-		hyperrobot(slack);	
+		hyperrobot(slack);
 	});
 	describe('base', () => {
 		it('responds to ハイパーロボット', async () => {
 			cloudinary.url = 'https://hoge.com/hoge.png';
 			const {username, attachments, text,} = await slack.getResponseTo('ハイパーロボット 3手');
-		
+
 			expect(username).toBe('hyperrobot');
 			expect(text).toContain('3手詰めです');
 			expect(attachments).toHaveLength(1);
-		});
+		}, 60000);
 	});
 	describe('battle', () => {
 		it('responds to ハイパーロボットバトル', async () => {
 			cloudinary.url = 'https://hoge.com/hoge.png';
 			const {username, attachments, text,} = await slack.getResponseTo('ハイパーロボットバトル');
-		
+
 			expect(username).toBe('hyperrobot');
 			expect(text).toContain(':question:手詰めです');
 			expect(attachments).toHaveLength(1);
-		});
+		}, 60000);
 		it('responds to first bidding', async () => {
 			const {username, text,} = await slack.getResponseTo('3');
 			expect(username).toBe('hyperrobot');
