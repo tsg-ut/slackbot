ee67cfc52a8f790d0e2811bd910346936fa8609c
Koki Takahashi <hakatasiloving@gmail.com>
2019-03-10 04:20:31 +0900

    deploy: Update command to update git submodules

--- deploy/index.ts
+++ deploy/index.ts
@@ -11,6 +11,7 @@ import {PassThrough} from 'stream';
 const commands = [
 	['git', 'checkout', '--', 'package.json', 'package-lock.json'],
 	['git', 'pull'],
+	['git', 'submodule', 'update', '--init', '--recursive'],
 	['npm', 'install', '--production'],
 ];
 
