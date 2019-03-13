d65c42e1586aa79dea1111b809413a1fff23313b
Koki Takahashi <hakatasiloving@gmail.com>
2019-03-13 19:11:33 +0900

    npx eslint --fix scrapbox/*.ts

--- scrapbox/index.ts
+++ scrapbox/index.ts
@@ -9,11 +9,11 @@ const getScrapboxUrl = (pageName: string) => `https://scrapbox.io/api/pages/tsg/
 
 // slack-log
 const slacklogAPIDomain = 'localhost:9292';
-const slacklogURLRegexp = RegExp(`^https?://slack-log.tsg.ne.jp/([A-Z0-9]+)/([0-9]+\.[0-9]+)`);
+const slacklogURLRegexp = RegExp('^https?://slack-log.tsg.ne.jp/([A-Z0-9]+)/([0-9]+\.[0-9]+)');
 const getAroundMessagesUrl = (channel: string) => `http://${slacklogAPIDomain}/around_messages/${channel}.json`;
 
 export const server = () => async (fastify: FastifyInstance) => {
-    // slack-log
+	// slack-log
 	const users = await axios.get(`http://${slacklogAPIDomain}/users.json`).then(({data}) => data);
 	const channels = await axios.get(`http://${slacklogAPIDomain}/channels.json`).then(({data}) => data);
 
@@ -31,61 +31,67 @@ export const server = () => async (fastify: FastifyInstance) => {
 				req.body.event.links.map((link: string) => logger.info('-', link));
 				const unfurls: LinkUnfurls = {};
 				for (const link of req.body.event.links) {
-					const { url, domain } = link;
-                    switch (domain) {
-                        case 'scrapbox.io':
-                            {
-                                if (!/^https?:\/\/scrapbox.io\/tsg\/.+/.test(url)) continue;
-                                let pageName = url.replace(/^https?:\/\/scrapbox.io\/tsg\/(.+)$/, '$1');
-                                try {
-                                    if (decodeURI(pageName) === pageName) {
-                                        pageName = encodeURI(pageName);
-                                    }
-                                } catch {}
-                                const scrapboxUrl = getScrapboxUrl(pageName);
-                                const response = await axios.get(scrapboxUrl, { headers: { Cookie: `connect.sid=${process.env.SCRAPBOX_SID}` } });
-                                const data = response.data;
+					const {url, domain} = link;
+					switch (domain) {
+						case 'scrapbox.io':
+						{
+							if (!(/^https?:\/\/scrapbox.io\/tsg\/.+/).test(url)) {
+								continue;
+							}
+							let pageName = url.replace(/^https?:\/\/scrapbox.io\/tsg\/(.+)$/, '$1');
+							try {
+								if (decodeURI(pageName) === pageName) {
+									pageName = encodeURI(pageName);
+								}
+							} catch {}
+							const scrapboxUrl = getScrapboxUrl(pageName);
+							const response = await axios.get(scrapboxUrl, {headers: {Cookie: `connect.sid=${process.env.SCRAPBOX_SID}`}});
+							const {data} = response;
 
-                                unfurls[url] = {
-                                    title: data.title,
-                                    title_link: url,
-                                    author_name: 'Scrapbox',
-                                    author_icon: 'https://scrapbox.io/favicon.ico',
-                                    text: data.descriptions.join('\n'),
-                                    color: '#484F5E',
-                                    ...(data.image ? { image_url: data.image } : {}),
-                                };
-                                break;
-                            }
-                        case 'slack-log.tsg.ne.jp':
-                            {
-                                if (!slacklogURLRegexp.test(url)) continue;
+							unfurls[url] = {
+								title: data.title,
+								title_link: url,
+								author_name: 'Scrapbox',
+								author_icon: 'https://scrapbox.io/favicon.ico',
+								text: data.descriptions.join('\n'),
+								color: '#484F5E',
+								...(data.image ? {image_url: data.image} : {}),
+							};
+							break;
+						}
+						case 'slack-log.tsg.ne.jp':
+						{
+							if (!slacklogURLRegexp.test(url)) {
+								continue;
+							}
 
-                                const [_, chanid, ts] = slacklogURLRegexp.exec(url);
+							const [_, chanid, ts] = slacklogURLRegexp.exec(url);
 
-                                const aroundMessagesUrl = getAroundMessagesUrl(chanid);
-                                const response = await axios.post(aroundMessagesUrl, qs.stringify({ts}));
-                                // @ts-ignore
-                                const message = response.data.messages.find(m => m.ts === ts);
-                                if (!message) continue;
-                                const {text, user: userid} = message;
-                                const user = userid && users[userid];
-                                const username = user && user.name;
-                                const channel = chanid && channels[chanid];
-                                const channame = channel && channel.name
-                                const imageUrl = user && user.profile && (user.profile.image_original || user.profile.image_512);
+							const aroundMessagesUrl = getAroundMessagesUrl(chanid);
+							const response = await axios.post(aroundMessagesUrl, qs.stringify({ts}));
+							// @ts-ignore
+							const message = response.data.messages.find((m) => m.ts === ts);
+							if (!message) {
+								continue;
+							}
+							const {text, user: userid} = message;
+							const user = userid && users[userid];
+							const username = user && user.name;
+							const channel = chanid && channels[chanid];
+							const channame = channel && channel.name;
+							const imageUrl = user && user.profile && (user.profile.image_original || user.profile.image_512);
 
-                                unfurls[url] = {
-                                    color: '#4D394B',
-                                    author_name: username || userid,
-                                    author_icon: imageUrl || ':void:',
-                                    text,
-                                    footer: `Posted in #${channame || `<${channel}>`}`,
-                                    ts,
-                                };
-                                break;
-                            }
-                    }
+							unfurls[url] = {
+								color: '#4D394B',
+								author_name: username || userid,
+								author_icon: imageUrl || ':void:',
+								text,
+								footer: `Posted in #${channame || `<${channel}>`}`,
+								ts,
+							};
+							break;
+						}
+					}
 				}
 				if (Object.values(unfurls).length > 0) {
 					try {
