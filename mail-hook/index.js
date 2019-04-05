edc728b4d3010e864f9bcfe21a33920067fb5b19
cookie-s <cookie-s@users.noreply.github.com>
2019-04-05 11:08:26 +0900

    mail-hook: log error

--- mail-hook/index.js
+++ mail-hook/index.js
@@ -1,3 +1,4 @@
+const logger = require('../lib/logger.js');
 const isValidUTF8 = require('utf-8-validate');
 
 const sanitizeCode = (input) => ["`", input.replace(/`/g, "'"), "`"].join('');
@@ -35,6 +36,8 @@ module.exports.server = ({webClient: slack}) => async (fastify) => {
 
             return res.send('ok');
         } catch (e) {
+            logger.error('mail-hook error:', e);
+
             await slack.chat.postMessage({
                 channel: process.env.CHANNEL_PRLOG,
                 username: 'Email Notifier',
