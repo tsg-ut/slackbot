80db53fb060eedab567b749beb08535e82756ba0
Koki Takahashi <hakatasiloving@gmail.com>
2019-04-11 18:09:06 +0900

    WIP implementation of generateBoard

--- wordhero/index.ts
+++ wordhero/index.ts
@@ -1,8 +1,9 @@
 import fs from 'fs';
 import {promisify} from 'util';
 import path from 'path';
+import assert from 'assert';
 import {WebClient, RTMClient} from '@slack/client';
-import {flatten, maxBy} from 'lodash';
+import {flatten, maxBy, sample, random} from 'lodash';
 // @ts-ignore
 import trie from 'trie-prefix-tree';
 
@@ -75,6 +76,32 @@ const getWords = (tree: any, letters: string[]) => {
 	return Array.from(set);
 };
 
+const generateBoard = (tree: any, seed: string) => {
+	assert(seed.length <= 10);
+	let board = null;
+	while (board === null) {
+		const tempBoard = Array(16).fill(null);
+	    let pointer = random(0, 15);
+		let failed = false;
+		for (const index of Array(seed.length).keys()) {
+		    tempBoard[pointer] = seed[index];
+			if (index !== seed.length - 1) {
+				const precedings = precedingsList[pointer].filter((cell) => tempBoard[cell] === null);
+				if (precedings.length === 0) {
+					failed = true;
+					break;
+				}
+				pointer = sample(precedings);
+			}
+		}
+		if (!failed) {
+			board = tempBoard;
+		}
+	}
+	console.log(seed, board);
+	return board;
+};
+
 export default async ({rtmClient: rtm, webClient: slack}: SlackInterface) => {
 	const data = await Promise.all([
 		promisify(fs.readFile)(path.join(__dirname, '..', 'tahoiya', 'wikipedia.txt')),
@@ -84,8 +111,19 @@ export default async ({rtmClient: rtm, webClient: slack}: SlackInterface) => {
 		promisify(fs.readFile)(path.join(__dirname, '..', 'tahoiya', 'ewords.txt')),
 		promisify(fs.readFile)(path.join(__dirname, '..', 'tahoiya', 'fideli.txt')),
 	]);
-	const dictionary = new Set(flatten(data.map((datum) => datum.toString().split('\n').map((line) => line.split('\t')[1]))));
-	const tree = trie(Array.from(dictionary).filter((s) => typeof s === 'string'));
+	const dictionary = Array.from(new Set(flatten(data.map((datum) => (
+		datum.toString().split('\n').map((line) => line.split('\t')[1])
+	))))).filter((s) => (
+		typeof s === 'string' && 2 <= s.length && s.length <= 16
+	));
+	const seedWords = dictionary.filter((word) => 8 <= word.length && word.length <= 10);
+	console.time('tree');
+	const tree = trie(dictionary);
+	console.timeEnd('tree');
+	console.time('lightTree');
+	const lightTree = trie(dictionary.filter((word) => word.length <= 5));
+	console.timeEnd('lightTree');
+	const board = generateBoard(lightTree, sample(seedWords));
 	const words = getWords(tree, 'んかううういんかいんいうんいかか'.split(''))
 	console.log(maxBy(words, (word) => word.length))
 };
