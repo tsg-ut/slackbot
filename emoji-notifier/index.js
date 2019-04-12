7ab013245b32026d0823293e8a2ae1154f9aba16
Taiyo Mizuhashi <hase.tekitou@gmail.com>
2019-04-13 01:29:25 +0900

    Switch the channel to sandbox (emoji-notifier)

--- emoji-notifier/index.js
+++ emoji-notifier/index.js
@@ -6,7 +6,7 @@ module.exports = (clients) => {
 	rtm.on('emoji_changed', async (data) => {
 		if (data.subtype === 'add') {
 			const message = await slack.chat.postMessage({
-				channel: process.env.CHANNEL_RANDOM,
+				channel: process.env.CHANNEL_SANDBOX,
 				text: stripIndent`
 					絵文字 \`:${data.name}:\` が追加されました :+1::tada::muscle::raised_hands::innocent:
 				`,
@@ -23,7 +23,7 @@ module.exports = (clients) => {
 
 		if (data.subtype === 'remove') {
 			await slack.chat.postMessage({
-				channel: process.env.CHANNEL_RANDOM,
+				channel: process.env.CHANNEL_SANDBOX,
 				text: stripIndent`
 					絵文字 ${data.names.map((name) => `\`:${name}:\``).join(' ')} が削除されました :cry::broken_heart::x::fearful::innocent:
 				`,
