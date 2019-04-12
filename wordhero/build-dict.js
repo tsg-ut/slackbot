be2112e13f0788ce1d9092fb722dd105755c3044
Koki Takahashi <hakatasiloving@gmail.com>
2019-04-12 16:18:04 +0900

    Exclude dict directory from checkout

--- wordhero/build-dict.js
+++ wordhero/build-dict.js
@@ -4,6 +4,7 @@ const parse = require('csv-parse');
 const {promisify} = require('util');
 const fs = require('fs');
 const iconv = require('iconv-lite');
+const {hiraganize} = require('japanese');
 
 (async () => {
 	/*
@@ -23,13 +24,26 @@ const iconv = require('iconv-lite');
 	const encoder = iconv.encodeStream('UTF-8');
 	const reader = fs.createReadStream('dict/mecab-naist-jdic-0.6.3b-20111013/naist-jdic.csv');
 
-	let cnt  = 0
+	const dictionary = new Map();
+
 	parser.on('data', ([表層形, , , , 品詞1, 品詞2, 品詞3, 品詞4, 活用, 活用形, 基本形, 読み, 発音]) => {
-		if (品詞1 === '形容詞' && 活用形 !== '基本形') {
-		console.log({表層形, 品詞1, 品詞2, 品詞3, 品詞4, 活用, 活用形, 基本形, 読み, 発音});
-		cnt++;
+		if (品詞1 === '名詞') {
+			dictionary.set(hiraganize(読み), 表層形);
 		}
-		cnt >= 10 && process.exit();
+		if (品詞1 === '動詞' && 活用形 !== '基本形') {
+			dictionary.set(hiraganize(読み), 表層形);
+		}
+		if (品詞1 === '形容詞' && 活用形 !== '基本形') {
+			dictionary.set(hiraganize(読み), 表層形);
+		}
+	});
+
+	parser.on('end', () => {
+		console.log(dictionary.size);
+		const writer = fs.createWriteStream('dict/naistdic.tsv');
+		for (const [表層形, 読み] of dictionary.entries()) {
+			writer.write(`${表層形.replace(/\t/g, '')}\t${読み.replace()}\n`)
+		}
 	});
 
 	reader.pipe(decoder).pipe(encoder).pipe(parser);
