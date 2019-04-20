b8f5e1badd92b142a964281aceaf28df3c22138d
kurgm <gogottt2009-hp@yahoo.co.jp>
2019-04-20 17:05:18 +0900

    ahokusa: Add board initialization tests

--- ahokusa/index.test.js
+++ ahokusa/index.test.js
@@ -19,4 +19,40 @@ describe('ahokusa', () => {
 		expect(text).toContain(':void:');
 		expect(text).toMatch(/^(:[a-z-]+:\n?){6}$/);
 	});
+
+	it('accepts valid board initialization by emojis', async () => {
+		const board = [
+			':void::ahokusa-bottom-center::ahokusa-top-center:',
+			':ahokusa-bottom-left::ahokusa-top-left::ahokusa-top-right:',
+		].join('\n');
+
+		const {text, username} = await slack.getResponseTo(`@ahokusa ${board}`);
+
+		expect(username).toBe('ahokusa');
+		expect(text).toBe(board);
+	});
+
+	it('accepts valid board initialization by letters', async () => {
+		const {text, username} = await slack.getResponseTo(`@ahokusa .#_さくあ`);
+
+		expect(username).toBe('ahokusa');
+		expect(text).toBe([
+			':void::ahokusa-bottom-center::ahokusa-top-center:',
+			':ahokusa-bottom-left::ahokusa-top-left::ahokusa-top-right:',
+		].join('\n'));
+	});
+
+	it('rejects invalid board initialization', async () => {
+		const {text, username} = await slack.getResponseTo(`@ahokusa ああああああ`);
+
+		expect(username).toBe('ahokusa');
+		expect(text).toBe(':ha:');
+	});
+
+	it('rejects invalid board initialization', async () => {
+		const {text, username} = await slack.getResponseTo(`@ahokusa .#_さくああああああああああああああああああああ`);
+
+		expect(username).toBe('ahokusa');
+		expect(text).toBe(':ha:');
+	});
 });
