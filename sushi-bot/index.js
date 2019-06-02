4a5434cdb4adbd258b241d03a6ed2cdbb99f0630
Szkieletor-wsl-ubuntu <akitosano0111@gmail.com>
2019-06-03 00:17:05 +0900

    add: Implement '凍結ランキング 確認'

--- sushi-bot/index.js
+++ sushi-bot/index.js
@@ -124,9 +124,18 @@ module.exports = (clients) => {
                     currentRank++;
                 }
             }
+            if (tokens[0] === '凍結ランキング' && tokens[1] === '確認') {
+                let currentRank = 1;
+                for (let entry of suspendCounter.entries()) {
+                    if (entry[0] === user) {
+                        return postDM(`あなたの凍結回数は${entry[1]}個、現在の順位は${currentRank}位`);
+                    }
+                    currentRank++;
+                }
+            }
         }
 
-        let rtext = text;
+      let rtext = text;
         rtext = rtext.
             replace(/鮨/g, 'すし').
             replace(/(su|zu|ス|ズ|ず|寿|壽)/gi, 'す').
