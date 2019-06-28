14cc380c396794c8493b38aabe11e09e4dc2f421
kurgm <gogottt2009-hp@yahoo.co.jp>
2019-06-28 12:07:22 +0900

    voiperrobot: Use tabs

--- voiperrobot/index.test.ts
+++ voiperrobot/index.test.ts
@@ -5,22 +5,22 @@ import Slack from '../lib/slackMock.js';
 let slack: Slack = null;
 
 beforeEach(() => {
-  slack = new Slack();
-  process.env.CHANNEL_SANDBOX = slack.fakeChannel;
-  voiperrobot(slack);
+	slack = new Slack();
+	process.env.CHANNEL_SANDBOX = slack.fakeChannel;
+	voiperrobot(slack);
 });
 
 describe('voiperrobot', () => {
-  it('reponds to ボイパーロボット', async () => {
+	it('reponds to ボイパーロボット', async () => {
 		const {text, username} = await slack.getResponseTo('ボイパーロボット');
 
 		expect(username).toBe('voiperrobot');
 		expect(text).toMatch(/(はっ|ひっ|くっ|むか|つく|パン|ツか){8}/);
-  });
-  it('reponds to ボイパーロボットバトル', async () => {
+	});
+	it('reponds to ボイパーロボットバトル', async () => {
 		const {text, username} = await slack.getResponseTo('ボイパーロボットバトル');
 
 		expect(username).toBe('voiperrobot');
 		expect(text).toContain('ボイパーロボットバトルをはじめるよ〜');
-  });
+	});
 });
