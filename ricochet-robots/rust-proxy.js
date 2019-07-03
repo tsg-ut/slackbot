2f53e0a76fb57d124432c3b90cc65337c2d2c140
satos---jp <syogo8118@yahoo.co.jp>
2019-07-03 07:00:11 -0700

    change rust executable name to avoid conflict

--- ricochet-robots/rust-proxy.js
+++ ricochet-robots/rust-proxy.js
@@ -7,9 +7,7 @@ const board = require('./board.js');
 
 module.exports.get_data = async (depth) => {
 	if(!depth)depth = 20;
-	const generator = child_process.spawn(`target/release/main`, [`${depth}`]);
-	//console.log(generator);
-	//console.log(concatstream,concatstream.concat);
+	const generator = child_process.spawn(path.join(__dirname,'../target/release/ricochet_robot_problem_generator'), [`${depth}`]);
 	const output = await new Promise((resolve) => {
 		generator.stdout.pipe(concatstream({encoding: 'buffer'}, (data) => {
 			resolve(data);
