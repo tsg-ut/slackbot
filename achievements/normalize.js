4971744d0ad95a733269d3f74c831b5dbd147997
Koki Takahashi <hakatasiloving@gmail.com>
2019-07-17 14:33:55 +0900

    Update normalize.js to set first blood user for each achievement_data

--- achievements/normalize.js
+++ achievements/normalize.js
@@ -1,15 +1,20 @@
 const db = require('../lib/firestore.ts').default;
 const countBy = require('lodash/countBy');
 const groupBy = require('lodash/groupBy');
+const minBy = require('lodash/minBy');
 
 db.runTransaction(async (transaction) => {
 	const achievements = await transaction.get(db.collection('achievements'));
 	const rawAchievementData = await transaction.get(db.collection('achievement_data'));
 	const achievementData = new Map(rawAchievementData.docs.map((data) => [data.get('id'), data]));
 
-	const counts = countBy(achievements.docs, (achievement) => achievement.get('name'));
-	for (const [name, count] of Object.entries(counts)) {
-		await transaction.update(db.collection('achievement_data').doc(name), {count});
+	const names = groupBy(achievements.docs, (achievement) => achievement.get('name'));
+	for (const [name, nameAchievements] of Object.entries(names)) {
+		const first = minBy(nameAchievements, (achievement) => achievement.get('date'));
+		await transaction.update(db.collection('achievement_data').doc(name), {
+			count: nameAchievements.length,
+			first: first.get('user'),
+		});
 	}
 
 	const users = groupBy(achievements.docs, (achievement) => achievement.get('user'));
