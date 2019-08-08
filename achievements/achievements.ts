951972a32bae8b00fb24ebc6808a09144d6b9dbd
Koki Takahashi <hakatasiloving@gmail.com>
2019-08-08 18:26:33 +0900

    achievements: Add achievement for +:pwn: x5

--- achievements/achievements.ts
+++ achievements/achievements.ts
@@ -574,6 +574,15 @@ const achievements: Achievement[] = [
 		reaction: 'kurotorianzu',
 		value: 5,
 	},
+	{
+		id: 'reaction-pwn-5',
+		difficulty: 'hard',
+		title: 'pwnpwnpain',
+		condition: '1つのメッセージに対し5人以上から:pwn:リアクションをもらう',
+		category: 'reactions',
+		reaction: 'pwn',
+		value: 5,
+	},
 
 	// sushi-bot
 
