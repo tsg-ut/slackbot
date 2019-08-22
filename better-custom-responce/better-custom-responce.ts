f6fcaad58894cb84b018bc2f061c9cde71ea0c67
naan4UGen <47272321+naan4UGen@users.noreply.github.com>
2019-08-23 04:53:58 +0900

    more soreppoku

--- better-custom-responce/better-custom-responce.ts
+++ better-custom-responce/better-custom-responce.ts
@@ -1,5 +1,14 @@
 import customResponces from "./custom-responces";
 import {WebClient, RTMClient, MessageAttachment} from '@slack/client';
+import {Deferred} from '../lib/utils';
+
+interface SlackInterface {
+    rtmClient: RTMClient,
+    webClient: WebClient,
+    messageClient: any,
+}
+
+const loadDeferred = new Deferred();
 
 const responce = (text:string) => {
     const splittedText = text.replace(/ .,;:+\*<>?_}@\[`{]!"#\$%&'\(\)=~|-\^¥\\/g, ' ').split(' ');
@@ -25,9 +34,9 @@ export default async ({rtmClient: rtm, webClient: slack, messageClient: slackInt
 
     rtm.on('message', async (message) => {
         const {text} = message;
-        if(!text)return;
+        if (!text) return;
         const resp = responce(text);
-        if(!resp)return;
+        if (!resp) return;
         await slack.chat.postMessage({
             channel: message.channel,
             text: resp,
