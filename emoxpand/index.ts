7aef6eb85ec7db88f5e80d165975c02568fea388
KPCCoiL <copernicium@me.com>
2019-08-23 15:59:40 +0900

    implement emoji expansion

--- emoxpand/index.ts
+++ emoxpand/index.ts
@@ -5,7 +5,8 @@ import logger from '../lib/logger.js';
 import {RTMClient, WebClient} from '@slack/client';
 //@ts-ignore
 import {stripIndent} from 'common-tags';
-import {_} from 'lodash';
+import * as _ from 'lodash';
+import plugin from 'fastify-plugin';
 
 type EmojiName = string;
 type EmojiContent = EmojiName[][];
@@ -22,9 +23,12 @@ const emojiFromContent = (content: EmojiContent): BigEmoji => (
   {
     content,
     height: content.length,
-    width: Math.max(...content.map(x => x.length));
+    width: Math.max(...content.map(x => x.length)),
   }
 );
+
+const bigemojify = (smallEmoji: EmojiName): BigEmoji =>
+  emojiFromContent([[smallEmoji]]);
 
 const emojiData = 'bigemojis.json';
 const emojiPath = path.resolve(__dirname, emojiData);
@@ -66,19 +70,88 @@ const addEmoji = (name: EmojiName, emoji: BigEmoji): void => {
   storeEmojis(emojis);
 };
 
+const alignEmojis = (es: BigEmoji[]): string[] => {
+  if (es.length === 0) return [''];
+  const wholeHeight = Math.max(...es.map(e => e.height));
+  const voids = (width: number) => Array(width).fill('_');
+  const filled = es
+    .map(({width, height, content}) =>
+      content
+        .concat(Array(wholeHeight - height).fill(0).map(x => []))
+        .map(row => row
+          .concat(Array(width - row.length).fill('_'))
+          .join('')));
+  return _.zipWith(...filled, (...rows) => rows.join(''));
+};
+
+type TokenType = 'Plain' | 'Emoji' | 'BigEmoji';
+interface Token {
+  kind: TokenType
+  content: string;
+}
+
+
+const expandEmoji = (text: string): string =>
+  _.flatMap(text.split('\n'), line => {
+    const tokens: Token[] = (line + '\n').split('').reduce<[TokenType, string[], Token[]]>(
+      ([parsing, chars, parsed], ch) => {
+        const push = () => {
+          parsed.push({
+            kind: parsing,
+            content: chars.join(''),
+          });
+        };
+        switch (ch) {
+          case ':':
+            if (parsing === 'Plain') {
+              push();
+              return ['Emoji', [], parsed];
+            }
+            else if (parsing === 'Emoji') {
+              push();
+              return ['Plain', [], parsed];
+            }
+          case '!':
+            if (parsing === 'Plain') {
+              push();
+              return ['BigEmoji', [], parsed];
+            }
+            else if (parsing === 'BigEmoji') {
+              push();
+              return ['Plain', [], parsed];
+            }
+          case '\n':
+            push();
+            return ['Plain', [], parsed];
+        }
+        chars.push(ch);
+        return [parsing, chars, parsed];
+      },
+      ['Plain', [], []])[2];
+    if (tokens[tokens.length - 1].kind !== 'Plain')
+      tokens.push({ kind: 'Plain', content: '', });
+    return tokens.reduce(
+      ([inLine, lines], tok: Token) => {
+        if (tok.kind === 'Plain') {
+          const emojiStr = alignEmojis(
+            inLine.map(({kind, content}) => 
+              kind === 'Emoji' ? bigemojify(content) : emojis.get(content)))
+          emojiStr[0] += tok.content;
+          lines.push(emojiStr.join('\n'));
+          return [[], lines];
+        }
+        inLine.push(tok);
+        return [inLine, lines];
+      },
+      [[], []])[1];
+  }).join('\n');
+
 interface SlackInterface {
   rtmClient: RTMClient;
   webClient: WebClient;
 }
 
-interface WaitingContent {
-  name: EmojiName;
-}
-type RegistrationState = 'WaitingRegistration' | 'WaitingName' | WaitingContent;
-
-let state: RegistrationState = 'WaitingRegistration';
-
-export default ({rtmClient: rtm, webClient: slack}: SlackInterface) => {
+export const server = ({rtmClient: rtm, webClient: slack}: SlackInterface) => plugin(async (fastify, opts, next) => {
   const postMessage = (text: string): void => {
     slack.chat.postMessage({
       channel: process.env.CHANNEL_SANDBOX,
@@ -87,12 +160,21 @@ export default ({rtmClient: rtm, webClient: slack}: SlackInterface) => {
       icon_emoji: ':chian-ga-aru:',
     });
   };
+  // Big Emoji expansion {{{
+
+  // }}}
   
-  const is_in_sandbox = (message: any) =>
-    message.channel === process.env.CHANNEL_SANDBOX;
+  // Big Emoji registration {{{
+  interface WaitingContent {
+    name: EmojiName;
+  }
+
+  type RegistrationState = 'WaitingRegistration' | 'WaitingName' | WaitingContent;
+
+  let state: RegistrationState = 'WaitingRegistration';
 
   rtm.on('message', async message => {
-    if (!is_in_sandbox(message))
+    if (message.channel !== process.env.CHANNEL_SANDBOX)
       return;
 
     if (message.text === 'やーめた') {
@@ -125,13 +207,15 @@ export default ({rtmClient: rtm, webClient: slack}: SlackInterface) => {
             .map(row =>
               row.slice(1, row.length - 1).split('::'));
           addEmoji(state.name, emojiFromContent(content));
-          postMessage(stripIndent`
-大絵文字 \`!${state.name}!\`
+          postMessage(String.raw`大絵文字 \`!${state.name}!\`
 ${match[1]}
 が登録されました:sushi-go-left::waiwai::saikou::chian-ga-aru::sushi-go-right:`);
           state = 'WaitingRegistration';
         }
         break;
     }
+    // }}}
+
   });
-};
+});
+
