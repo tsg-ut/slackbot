653a33407235f28698244a830de23cac01c257a6
Koki Takahashi <hakatasiloving@gmail.com>
2019-09-03 21:42:14 +0900

    Fix for 〇〇しています

--- better-custom-response/responses/hontouni.ts
+++ better-custom-response/responses/hontouni.ts
@@ -12,16 +12,17 @@ export default async ([text, tail]: string[]): Promise<string[]> => {
         basic_form !== 'ちゃう' &&
         basic_form !== 'た'
     ));
-    const startPosition = findLastIndex(tokens, ({pos_detail_1}) => (
-        pos_detail_1 === '自立'
+    const startPosition = findLastIndex(tokens, ({pos, pos_detail_1, basic_form}) => (
+        (basic_form !== 'する' && pos_detail_1 === '自立') ||
+        pos === '名詞'
     ), endPosition);
     const 語幹Tokens = tokens.slice(
-      startPosition === -1 ? 0 : startPosition,
-      endPosition === -1 ? -3 : endPosition + 1,
+        startPosition === -1 ? 0 : startPosition,
+        endPosition === -1 ? -3 : endPosition + 1,
     );
     const 語幹 = 語幹Tokens.map(({surface_form}) => surface_form).join('');
     if (tail[0] === 'て' || tail[0] === 'ち') {
-      return [`本当に${語幹}ていますか？`];
+        return [`本当に${語幹}ていますか？`];
     }
     return [`本当に${語幹}でいますか？`];
 };
