a93f1b8218e8a6d0e9186a8da94bb705aa6d774c
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2019-09-04 23:58:49 +0900

    bcr: implement multiple inputs

--- better-custom-response/web/src/index.js
+++ better-custom-response/web/src/index.js
@@ -3,88 +3,111 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 class Response extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      input: null,
-      output: null,
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputs: [''],
+            output: null,
+        };
+    }
+    
+    send = (event) => {
+        console.log('send data:');
+        console.log(this.state);
+        axios.post(`http://localhost:3001/bcr/update`, this.state);
+    }
+    
+    handleTextChange = (event, name, i) => {
+        const data = this.state[name].slice();
+        data[i] = event.target.value;
+        this.setState({[name]: data});
+        console.log(name, this.state[name][i]);
     };
-  }
-
-  send = (event) => {
-    console.log('send data:');
-    console.log(this.state);
-    axios.post(`http://localhost:3001/bcr/update`, this.state);
-  }
-
-  handleTextChange = (event, type) => {
-    this.setState({[type]: event.target.value});
-    console.log(type, this.state[type]);
-  };
-
-  render() {
-    return (
-      <div>
-        <Inputs onChange={(event) => this.handleTextChange(event, 'input')}></Inputs>
-        <Outputs onChange={(event) => this.handleTextChange(event, 'output')}></Outputs>
-        <Options></Options>
-        <SaveButton onClick={this.send}></SaveButton>
-      </div>
-    );
-  }
+    
+    render() {
+        return (
+            <div>
+                <Inputs
+                    onChange={(event, i) => this.handleTextChange(event, 'inputs', i)}
+                    inputs={this.state.inputs}
+                ></Inputs>
+                <Outputs onChange={(event) => this.handleTextChange(event, 'output')}></Outputs>
+                <Options></Options>
+                <SaveButton onClick={this.send}></SaveButton>
+            </div>
+        );
+    }
 }
 
+
+class Input extends React.Component {
+    render() {
+        return (
+            <input type="text" onInput={this.props.onChange}></input>
+        )
+    }
+}
+    
 class Inputs extends React.Component {
-
-  render() {
-    return (
-      <input type="text" onInput={this.props.onChange}></input>
-    );
-  }
+    render() {
+        return (
+            <div>
+                {Object.keys(this.props.inputs).map((_, i) => 
+                    <Input onChange={(event) => this.props.onChange(event, i)}></Input>
+                )}
+                { // TODO: add button }
+            </div>
+        );
+    }
 }
-
+        
 class Outputs extends React.Component {
-  render() {
-    return (
-      <textarea onChange={this.props.onChange}></textarea>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <textarea onChange={this.props.onChange}></textarea>
+            </div>
+        );
+    }
 }
+            
 class Options extends React.Component {
-  render() {
-    return (
-      <div>
-        <Option></Option>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Option></Option>
+            </div>
+        );
+    }
 }
+                
 class Option extends React.Component {
-  render() {
-    return (
-      <input type="checkbox" ></input>
-    );
-  }
+    render() {
+        return (
+            <input type="checkbox" ></input>
+        );
+    }
 }
-
+                    
 class SaveButton extends React.Component {
-  render() {
-    return (
-      <button onClick={this.props.onClick}>save</button>
-    );
-  }
+    render() {
+        return (
+            <button onClick={this.props.onClick}>save</button>
+        );
+    }
 }
-
+                        
 const App = () => {
-  return (
-    <div>
-      <header>
-        <p>カスタムレスポンス</p>
-      </header>
-      <Response></Response>
-    </div>
-  );
+    return (
+        <div>
+            <header>
+                <p>カスタムレスポンス</p>
+            </header>
+            <Response></Response>
+        </div>
+    );
 }
-
+                            
 ReactDOM.render(<App />, document.getElementById('root'));
-
+                            
+                            
