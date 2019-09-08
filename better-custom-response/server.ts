a6dc37dffc4d8019f847f1a6477a97ede22b2b9d
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2019-09-08 13:34:07 +0900

    bcr: save responses on web

--- better-custom-response/server.ts
+++ better-custom-response/server.ts
@@ -1,6 +1,9 @@
 import {FastifyInstance} from 'fastify';
 import {CustomResponse} from './custom-responses';
 import {WebClient, RTMClient, MessageAttachment} from '@slack/client';
+import {promisify} from 'util';
+import {writeFile} from 'fs';
+import path from 'path';
 
 interface TextBoxContent {
     id: string,
@@ -13,13 +16,33 @@ interface WebCustomResponse {
     outputs: TextBoxContent[],
 }
 
-const customResponses= new Map<string, WebCustomResponse>();
+const savedResponsesPath = 'web-custom-responses.json';
+
+const customResponses: Map<string, WebCustomResponse> = (() => {
+    try {
+        const savedResponses: WebCustomResponse[] = require(`./${savedResponsesPath}`);
+        return new Map(savedResponses.map(r => [r.id, r]));
+    } catch (e) {
+        if(e.code === 'MODULE_NOT_FOUND') {
+            return new Map<string, WebCustomResponse>();
+        }
+        throw e;
+    }
+})();
+
+const saveResponses = async () => {
+    await promisify(writeFile)(
+        path.join(__dirname, savedResponsesPath),
+        JSON.stringify(Array.from(customResponses.values())),
+    );
+}
 
 export const server = ({webClient: slack}: {webClient: WebClient}) => async (fastify: FastifyInstance) => {
     fastify.post('/bcr/update', async (req, res) => {
         const {id, inputs, outputs}: {id: string, inputs: any[], outputs: any[]} = req.body;
         const response: WebCustomResponse = {id, inputs, outputs};
         customResponses.set(id, response);
+        saveResponses();
         return ({ok: true});
     });
 
