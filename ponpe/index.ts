2b381356d7a4231d38fad0a1beaee426181ec936
satos---jp <syogo8118@yahoo.co.jp>
2019-09-23 02:04:07 -0700

    update ponpe for easier hints

--- ponpe/index.ts
+++ ponpe/index.ts
@@ -1,6 +1,10 @@
 import {WebClient, RTMClient} from '@slack/client';
 import moment from 'moment';
 import axios from 'axios';
+// @ts-ignore
+import hiraganize from 'japanese';
+import sample from 'lodash/sample';
+import fs from 'fs';
 import {getMemberName} from '../lib/slackUtils';
 
 interface SlackInterface {
@@ -12,11 +16,13 @@ function getTimeLink(time:number){
 	return moment(time).utcOffset('+0900').format('HH:mm:ss');
 }
 
-async function getTheme(){
+
+async function getHardTheme(){
 	const header = await axios.head('https://www.weblio.jp/WeblioRandomSelectServlet');
 	//スペース(+) どうにかする
 	return decodeURI(header.request.path.split('/')[2]);
 }
+
 
 interface Hint{
 	data: string,
@@ -25,8 +31,13 @@ interface Hint{
 	user: string,
 }
 
+interface Theme{
+	word: string,
+	ruby: string,
+}
+
 interface State{
-	answer: string,
+	answer: Theme,
 	registerend: number,
 	registants: string[],
 	hints: Hint[],
@@ -44,6 +55,18 @@ export default async ({rtmClient: rtm, webClient: slack}: SlackInterface) => {
 	const custom_emoji_list = Object.keys((await slack.emoji.list({token: process.env.HAKATASHI_TOKEN})).emoji);
 	const emoji_list = default_emoji_list + custom_emoji_list;
 	
+	//cat BCCWJ_frequencylist_luw_ver1_0.tsv | grep "名詞" | grep -v "人名" | grep -v "数詞" 
+	//    | head -n 50000 | tail -n 10000 | awk '{ print $2 "," $3 }' > common_word_list
+	const themes = String(fs.readFileSync('../wiki_dataset/common_word_list')).split('\n');
+
+	function getTheme(){
+		const theme = sample(themes).split(',');
+		return {
+			word: theme[1],
+			ruby: hiraganize(theme[0]),
+		};
+	}
+	
 	rtm.on('message', async (message) => {
 		if(message.type !== 'message' || message.subtype === 'message_replied'){
 			return;
@@ -55,19 +78,19 @@ export default async ({rtmClient: rtm, webClient: slack}: SlackInterface) => {
 			});
 		}
 		
-		const answertime = 5 * 60 * 1000;
-		const registertime = 3 * 60 * 1000;
+		const answertime = 3 * 60 * 1000;
+		const registertime = 5 * 60 * 1000;
 		async function chainbids(){
 			if(states[0].hints.length > 0){
 				const endtime = Date.now() + answertime;
 				const hint = states[0].hints[0];
-				const msg = await reply(`${hint.data}\n${hint.user}さんのヒントだよ。${getTimeLink(endtime)}までにこのメッセージへのスレッドとして解答してね。もしこのヒントでわからずに諦める場合は「ギブアップ」と解答してね。`);
+				const msg = await reply(`${hint.data}\n${hint.user}さんのヒントだよ。${getTimeLink(endtime)}までにこのメッセージへのスレッドとしてひらがなで解答してね。文字数は${states[0].answer.ruby.length}文字だよ。もしこのヒントでわからずに諦める場合は「ギブアップ」と解答してね。`);
 				states[0].threadId = msg.ts;
 				states[0].timeoutId = setTimeout(chainbids, answertime);
 				states[0].hintuser = hint.user;
 			}
 			else{
-				await reply(`だれも正解できなかったよ:cry:。正解は「${states[0].answer}」だよ。`);
+				await reply(`だれも正解できなかったよ:cry:。正解は「${states[0].answer.ruby}」だよ。`);
 				states.shift();
 			}
 			states[0].hints.shift();
@@ -81,7 +104,7 @@ export default async ({rtmClient: rtm, webClient: slack}: SlackInterface) => {
 				await reply(`今は回答中だよ`);
 				return;				
 			}
-			await reply(`今の登録中のお題は${states.slice(-1)[0].answer}だよ！`);
+			await reply(`今の登録中のお題は「${states.slice(-1)[0].answer.ruby}」だよ！`);
 			states.slice(-1)[0].registants.push(message.user);
 		}
 		if(message.channel.startsWith('D') && message.text.startsWith('ぽんぺ登録')){
@@ -122,7 +145,7 @@ export default async ({rtmClient: rtm, webClient: slack}: SlackInterface) => {
 				time: Date.now(),
 				user: user,
 			});
-			await reply(`お題「${states.slice(-1)[0].answer}」に対してコスト${emoji_count}のぽんぺが登録されたよ:tada:`);
+			await reply(`お題「${states.slice(-1)[0].answer.ruby}」に対してコスト${emoji_count}のぽんぺが登録されたよ:tada:`);
 			states.slice(-1)[0].registants.push(message.user);
 			
 			await slack.chat.postMessage({
@@ -149,7 +172,7 @@ export default async ({rtmClient: rtm, webClient: slack}: SlackInterface) => {
 						thread_ts: states[0].threadId,
 					});
 				}
-				else if(message.text === states[0].answer){
+				else if(message.text === states[0].answer.ruby){
 					await slack.reactions.add({
 						name: 'tada',
 						channel: message.channel,
