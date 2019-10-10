f35c5107cc6d5b363db334499532ece79280a157
Koki Takahashi <hakatasiloving@gmail.com>
2019-10-10 18:56:38 +0900

    Update jest config to collect coverage from

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
 		'^.+\\.tsx?$': 'ts-jest',
 	},
 	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[tj]s$',
+	collectCoverageFrom: [
+		'**/*.{js,ts}',
+		'!**/*.test.{js,ts}',
+	],
+	coveragePathIgnorePatterns: [
+		'coverage',
+		'/node_modules/',
+	],
 };
