c07421113a7f8ceac427710197ba4f7288c82e75
hideo54 <hideo54@hideo54.com>
2020-09-19 17:10:55 +0900

    これや答えは俺の

--- lib/firestore-live5.ts
+++ lib/firestore-live5.ts
@@ -2,12 +2,12 @@ import * as firebase from 'firebase-admin';
 
 import serviceAccount from '../google_application_credentials_live5.json';
 
-firebase.initializeApp({
+const liveApp = firebase.initializeApp({
     // @ts-ignore
     credential: firebase.credential.cert(serviceAccount),
     databaseURL: process.env.FIREBASE_ENDPOINT_LIVE5,
 }, 'tsg-live-5');
 
-const db = firebase.firestore();
+const db = firebase.firestore(liveApp);
 
 export default db;
