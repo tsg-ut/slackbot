e2fcf1d9976b3e7d48f9893c895b4fb0da13f040
settyan117 <sute-kidaisuki@hotmail.co.jp>
2020-11-06 21:20:26 +0000

    [add] コメントを追加

--- achievement-quiz/index.ts
+++ achievement-quiz/index.ts
@@ -7,13 +7,13 @@ import { unlock } from '../achievements';
 import achievements, { Difficulty } from '../achievements/achievements';
 import assert from 'assert';
 
+// ゲームの状態を保持する変数のクラス
+// スレッド内のみ解答可能の場合は、thread_tsを保持するメンバも持つ
 class AchievementQuizState {
   answer: number[] = [];
-  thread?: string = undefined;
   inGame: boolean = false;
   clear() {
     this.answer = [];
-    this.thread = undefined;
     this.inGame = false;
     return;
   }
@@ -27,5 +27,44 @@ export default ({
   webClient: WebClient;
 }) => {
   const state = new AchievementQuizState();
-  rtm.on('message', async message => {});
+
+  rtm.on('message', async message => {
+    // sandboxでのみ
+    if (message.channel !== process.env.CHANNEL_SANDBOX) {
+      return;
+    }
+    // botを弾く
+    if (
+      message.subtype === 'bot_message' ||
+      message.subtype === 'slackbot_response'
+    ) {
+      return;
+    }
+    // undefined避け
+    if (!message.text) {
+      return;
+    }
+
+    // メッセージ内容が「実績当てクイズ」だった場合
+    if (message.text.match(/^実績当てクイズ$/)) {
+      // ゲーム進行中の場合
+      if (state.inGame) {
+        // 今回はただスルー
+        return;
+
+        // スレッド解答式の場合は、進行中のスレッド内でreply_broadcastをtrueにして通知すると良い
+        // （進行中のスレッドを探すのは、一般的にめんどくさいため）
+        /*
+        await slack.chat.postMessage({
+          text: '進行中のゲームがあるよ:thinking_face:',
+          channel: process.env.CHANNEL_SANDBOX as string,
+          username: '実績当てクイズ',
+          icon_emoji: 'achievement',
+          thread_ts: state.thread,
+          reply_broadcast: true,
+        });
+        */
+      }
+    }
+  });
 };
