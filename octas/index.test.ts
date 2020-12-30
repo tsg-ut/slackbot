f4b7d6135cfdf9668381b6e9200819cc12366026
Yosshi999 <Yosshi999@users.noreply.github.com>
2020-12-30 18:15:37 +0900

    strage indent

--- octas/index.test.ts
+++ octas/index.test.ts
@@ -8,16 +8,16 @@ import { List } from 'lodash';
 let slack: Slack = null;
 
 beforeEach(async () => {
-	slack = new Slack();
-	process.env.CHANNEL_SANDBOX = slack.fakeChannel;
-	await octas(slack);
+    slack = new Slack();
+    process.env.CHANNEL_SANDBOX = slack.fakeChannel;
+    await octas(slack);
 });
 
 describe('octas', () => {
     it('respond to octas', async () => {
-        const {channel, text, attachments}: {channel: string, text: string, attachments: List<any>} = await slack.getResponseTo('octas');
+        const { channel, text, attachments }: { channel: string, text: string, attachments: List<any> } = await slack.getResponseTo('octas');
 
-		expect(channel).toBe(slack.fakeChannel);
+        expect(channel).toBe(slack.fakeChannel);
         expect(text).toContain('Octas対人を始めるよ～');
         expect(attachments).toHaveLength(1);
     });
