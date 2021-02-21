384154ee40bb3af01712a542561ac672ebdad5d8
cookie-s <kcz@kcz.sh>
2021-02-22 00:44:43 +0900

    channel-notifier: use SDK for conversations.invite

--- channel-notifier/index.js
+++ channel-notifier/index.js
@@ -5,13 +5,10 @@ module.exports = (clients) => {
 	const {rtmClient: rtm, webClient: slack} = clients;
 
 	const notify = async ({type, channel, user}) => {
-		await axios.post('https://slack.com/api/conversations.invite', {
+		await slack.conversations.invite({
 			channel,
 			users: process.env.USER_TSGBOT, // A comma separated list of user IDs
-		}, {
-			headers: {
-				Authorization: `Bearer ${process.env.HAKATASHI_TOKEN}`,
-			},
+			token: process.env.HAKATASHI_TOKEN,
 		});
 
 		const verb = type === 'create' ? '作成' : 'アーカイブから復元';
