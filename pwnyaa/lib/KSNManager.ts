7f16c35e779a8edbe239fc16c6114bbe571ba3b1
ssmallkirby <ssmallkirby@gmail.com>
2021-03-13 10:46:57 +0900

    pwnyaa: cache challs of ksnctf
    - ksnctf doesn't have user-profile page, and the score of the
    challs are not shown in log-page. Therefore, chache data of challs
    in the module and reuse it for better appearance of user profiles.
    The cache is updated every 12 hours.

--- pwnyaa/lib/KSNManager.ts
+++ pwnyaa/lib/KSNManager.ts
@@ -3,6 +3,12 @@ import scrapeIt from 'scrape-it';
 import {Challenge, SolvedInfo, Profile} from './BasicTypes';
 
 const SAFELIMIT = 20;
+const CACHE_UPDATE_INTERNAL = 12;
+
+let cachedChalls: {
+	updatedAt: Date,
+	challs: Challenge[],
+} = null;
 
 const getAxiosClientKSN = () => {
 	const clientKSN = axios.create();
@@ -26,7 +32,7 @@ const getDateKSN = (dateStr: string) => {
 const parseProfileKSN = (htmls: any[], userId: string) => {
 	const re: RegExp = /<li>([0-9]{4}\/[0-9]{2}\/[0-9]{2}) @([^ ]+) solved ([0-9]+) (.*?)<\/li>/g;
 	let results: RegExpExecArray = null;
-	const solvedChalls: SolvedInfo[] = [];
+	let solvedChalls: SolvedInfo[] = [];
 
 	for (const html of htmls) {
 		while ((results = re.exec(html)) !== null) {
@@ -44,10 +50,17 @@ const parseProfileKSN = (htmls: any[], userId: string) => {
 		return null;
 	}
 
+	if (cachedChalls !== null) {
+		solvedChalls = solvedChalls.map((chall) => ({
+			...chall,
+			score: cachedChalls.challs.find((c) => c.id === chall.id).score,
+		} as SolvedInfo));
+	}
+
 	const tmpscore = solvedChalls.reduce((total, chall) => total + Number(chall.score), 0);
 	const fetchedProfile: Profile = {
 		username: userId,
-		country: 'unknown',
+		country: 'JP',
 		rank: 'unknown',
 		score: tmpscore !== 0 ? String(tmpscore) : 'unknown',
 		comment: '',
@@ -57,9 +70,32 @@ const parseProfileKSN = (htmls: any[], userId: string) => {
 	return fetchedProfile;
 };
 
+const needCacheUpdate = function () {
+	if (cachedChalls === null) {
+		return true;
+	}
+	if (Date.now() - cachedChalls.updatedAt.getTime() >= CACHE_UPDATE_INTERNAL * 60 * 60 * 1000) {
+		return true;
+	}
+	return false;
+};
+
+//  ksnctf doesn't have user-profile. Therefore, you can't know
+// the score of challs the user solved. However, checking the score
+// everytime for each user is too heavy for the server of ksnctf.
+// Hence, you cache the challs and update it every 12 hours.
+const confirmChallsCache = async function() {
+	if (needCacheUpdate()) {
+		cachedChalls = {
+			updatedAt: new Date(),
+			challs: await fetchChallsKSN(),
+		};
+	}
+};
 
 export const fetchUserProfileKSN = async function(userId: string) {
 	const htmls = await fetchAllKSN();
+	await confirmChallsCache();
 	return parseProfileKSN(htmls, userId);
 };
 
