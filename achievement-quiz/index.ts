948fb50d0d8ea8393d8063530eea26d64cb55d24
settyan117 <sute-kidaisuki@hotmail.co.jp>
2021-04-04 17:46:39 +0000

    [update] 変わらないパラメータを組み込んだpostMessage関数を使うようにした

--- achievement-quiz/index.ts
+++ achievement-quiz/index.ts
@@ -2,13 +2,11 @@ import { RTMClient } from '@slack/rtm-api';
 import { WebClient } from '@slack/web-api';
 import { random } from 'lodash';
 import { stripIndent, source } from 'common-tags';
-import leven from 'leven';
 import { unlock } from '../achievements';
 import achievementsMap, {
   Achievement,
   Difficulty,
 } from '../achievements/achievements';
-import assert from 'assert';
 
 // ゲームの状態を保持する変数のクラス
 // スレッド内のみ解答可能の場合は、thread_tsを保持するメンバも持つ
@@ -48,25 +46,24 @@ export default ({
   const state = new AchievementQuizState();
   const achievements = Array.from(achievementsMap.values());
 
+  const postMessage = async (text: string, thread_ts?: string, reply_broadcast: boolean = false) => {
+    return await slack.chat.postMessage({
+      text,
+      channel: process.env.CHANNEL_SANDBOX as string,
+      username: '実績当てクイズ',
+      icon_emoji: 'achievement',
+      thread_ts,
+      reply_broadcast,
+    })
+  }
+
   // タイムアップ処理
   const timeUp = async () => {
-    await slack.chat.postMessage({
-      text: '～～～～～～～～～～おわり～～～～～～～～～～',
-      channel: process.env.CHANNEL_SANDBOX as string,
-      username: '実績当てクイズ',
-      icon_emoji: 'achievement',
-      thread_ts: state.thread,
-    });
-    await slack.chat.postMessage({
-      text: stripIndent`
-          正解者は出ませんでした:sob:
-          答えは *<https://achievements.tsg.ne.jp/achievements/${state.answer.id}|${state.answer.title}>* だよ:cry:`,
-      channel: process.env.CHANNEL_SANDBOX as string,
-      username: '実績当てクイズ',
-      icon_emoji: 'achievement',
-      thread_ts: state.thread,
-      reply_broadcast: true,
-    });
+    await postMessage('～～～～～～～～～～おわり～～～～～～～～～～', state.thread)
+    await postMessage(stripIndent`
+    正解者は出ませんでした:sob:
+    答えは *<https://achievements.tsg.ne.jp/achievements/${state.answer.id}|${state.answer.title}>* だよ:cry:`,
+    state.thread, true);
 
     // 終了処理
     state.clear();
@@ -92,47 +89,39 @@ export default ({
       if (state.inGame) {
         // 進行中のスレッド内でreply_broadcastをtrueにして通知
         // （進行中のスレッドを探すのは、一般的にめんどくさいため）
-        await slack.chat.postMessage({
-          text: '進行中のゲームがあるよ:thinking_face:',
-          channel: process.env.CHANNEL_SANDBOX as string,
-          username: '実績当てクイズ',
-          icon_emoji: 'achievement',
-          thread_ts: state.thread,
-          reply_broadcast: true,
-        });
+        await postMessage('進行中のゲームがあるよ:thinking_face:', state.thread, true)
+
       } else {
         state.inGame = true;
         state.answer = achievements[random(0, achievements.length - 1)];
-        const { ts } = await slack.chat.postMessage({
-          text: source`
-          この実績なーんだ（解答はスレッドへ）
-          >>>*解除条件*: ${state.answer.condition}
-          *解除難易度*: ${difficultyToStars(state.answer.difficulty)} (${
+        const titleHided = state.answer.title[0] + Array.from(state.answer.title.slice(1)).map(char => {
+          if (char.match(/^[\p{Letter}\p{Number}]$/u)) {
+            return '_';
+          }
+          else {
+            return char;
+          }
+        }).join('')
+        const { ts } = await postMessage(
+          source`
+          【${titleHided}】（${state.answer.title.length}文字）
+          >*解除条件*: ${state.answer.condition}
+          >*解除難易度*: ${difficultyToStars(state.answer.difficulty)} (${
             state.answer.difficulty
-          })`,
-          channel: process.env.CHANNEL_SANDBOX as string,
-          username: '実績当てクイズ',
-          icon_emoji: 'achievement',
-        });
+          })
+          解答はスレッドへ
+          `);
 
         state.thread = ts as string;
         state.timer = setTimeout(timeUp, 2 * 60 * 1000);
-        await slack.chat.postMessage({
-          text: `制限時間は2分です`,
-          channel: process.env.CHANNEL_SANDBOX as string,
-          username: '実績当てクイズ',
-          icon_emoji: 'achievement',
-          thread_ts: state.thread,
-        });
+        await postMessage('制限時間は2分です。\n雛形↓', state.thread);
+        await postMessage(titleHided, state.thread)
       }
     }
 
     // ゲーム中のスレッドでのみ反応
     if (message.thread_ts === state.thread) {
-      if (
-        leven(message.text, state.answer.title) <=
-        state.answer.title.length / 3
-      ) {
+      if (message.text === state.answer.title) {
         // リアクション
         await slack.reactions.add({
           name: 'thumbsup',
@@ -141,16 +130,11 @@ export default ({
         });
 
         // post
-        await slack.chat.postMessage({
-          text: stripIndent`
+        await postMessage(
+          stripIndent`
           <@${message.user}> 正解です:clap:
           答えは *<https://achievements.tsg.ne.jp/achievements/${state.answer.id}|${state.answer.title}>* だよ:laughing:`,
-          channel: process.env.CHANNEL_SANDBOX as string,
-          username: '実績当てクイズ',
-          icon_emoji: 'achievement',
-          thread_ts: state.thread,
-          reply_broadcast: true,
-        });
+          state.thread, true);
 
         // 終了処理
         state.clear();
