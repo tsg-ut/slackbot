ad620b1db5844bf5ef876cd846cc6e575a71d601
Koki Takahashi <hakatasiloving@gmail.com>
2021-04-05 12:32:56 +0900

    api: Implement return_to feature for root endpoint

--- api/index.ts
+++ api/index.ts
@@ -3,10 +3,27 @@ import Fastify from 'fastify';
 import logger from '../lib/logger.js';
 import {getAllMembers} from '../lib/slackUtils';
 
+interface RootRoute {
+	Querystring: {
+		return_to?: string,
+	},
+}
+
 export default () => {
 	const fastify = Fastify({
 		logger: true,
 		pluginTimeout: 50000,
+	});
+
+	fastify.get<RootRoute>('/', (request, reply) => {
+		if (request.query.return_to) {
+			const normalizedUrl = new URL(request.query.return_to.toString(), process.env.API_ENDPOINT).toString();
+			reply.redirect(normalizedUrl);
+			return;
+		}
+
+		reply.type('text/html');
+		reply.send(`<h1>You successfully authorized an access to the API endpoint of <a href="https://github.com/tsg-ut/slackbot">slackbot</a>!</h1>`);
 	});
 
 	fastify.get('/slack/users', async (request, reply) => {
