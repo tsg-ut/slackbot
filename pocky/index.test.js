2bfef7210574a522d4495115d2a167ce0165fade
settyan117 <sute-kidaisuki@hotmail.co.jp>
2021-04-09 11:56:49 +0900

    [fix] mocked ../lib/slackUtils

--- pocky/index.test.js
+++ pocky/index.test.js
@@ -2,6 +2,7 @@
 
 jest.mock('axios');
 jest.mock('../achievements');
+jest.mock('../lib/slackUtils')
 
 const axios = require('axios');
 const pocky = require('./index.js');
@@ -17,8 +18,8 @@ beforeEach(() => {
 
 describe('pocky', () => {
 	it('responds to "ほげ?"', async () => {
-		axios.response = {data: [null, ['ほげ ふが']]}
-		const {username, text} = await slack.getResponseTo('ほげ?');
+		axios.response = { data: [null, ['ほげ ふが']] }
+		const { username, text } = await slack.getResponseTo('ほげ?');
 
 		expect(username).toBe('pocky');
 		expect(text).toBe('ふが');
