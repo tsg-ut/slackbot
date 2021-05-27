b93241bb554235690a2c8ed42ed71240cc0c6203
Koki Takahashi <hakatasiloving@gmail.com>
2021-05-28 00:08:43 +0900

    discord: undebug

--- discord/tts.ts
+++ discord/tts.ts
@@ -154,7 +154,7 @@ export default class TTS extends EventEmitter {
 			const user = message.member.user.id;
 			const state = await this.state.load();
 
-			if (tokens[0]?.toUpperCase() === 'TTSDEV') {
+			if (tokens[0]?.toUpperCase() === 'TTS') {
 				if (tokens.length === 1 || tokens[1] === 'start') {
 					if (!this.users.has(user)) {
 						if (!{}.hasOwnProperty.call(state.userVoices, user)) {
