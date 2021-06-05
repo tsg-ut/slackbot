ed847d3f5e2471c508c8862978756e91721d48da
Koki Takahashi <hakatasiloving@gmail.com>
2021-06-05 18:22:48 +0900

    nojoin: Leave join message if it is posted outside sandbox

--- nojoin/index.ts
+++ nojoin/index.ts
@@ -2,7 +2,10 @@ import {SlackInterface} from '../lib/slack';
 
 export default async ({rtmClient: rtm, webClient: slack}: SlackInterface) => {
 	rtm.on('message', async (message: any) => {
-		if (message.subtype === 'channel_join' || message.subtype === 'channel_leave') {
+		if (
+			(message.subtype === 'channel_join' && message.channel === process.env.CHANNEL_SANDBOX) ||
+			message.subtype === 'channel_leave'
+		) {
 			await slack.chat.delete({
 				token: process.env.HAKATASHI_TOKEN,
 				channel: message.channel,
