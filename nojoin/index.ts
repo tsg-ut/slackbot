ac675e5c047668d128272f8b39c6029b42579862
Koki Takahashi <hakatasiloving@gmail.com>
2021-06-05 18:15:24 +0900

    nojoin: Remove limitation to limit nojoin bot target to sandbox

--- nojoin/index.ts
+++ nojoin/index.ts
@@ -1,22 +1,13 @@
-// Automatically removes join/leave messages in sandbox
-
-import {WebClient, RTMClient} from '@slack/client';
-
-interface SlackInterface {
-	rtmClient: RTMClient,
-	webClient: WebClient,
-}
+import {SlackInterface} from '../lib/slack';
 
 export default async ({rtmClient: rtm, webClient: slack}: SlackInterface) => {
-	rtm.on('message', async (message) => {
-		if (message.channel === process.env.CHANNEL_SANDBOX) {
-			if (message.subtype === 'channel_join' || message.subtype === 'channel_leave') {
-				await slack.chat.delete({
-					token: process.env.HAKATASHI_TOKEN,
-					channel: message.channel,
-					ts: message.ts,
-				});
-			}
+	rtm.on('message', async (message: any) => {
+		if (message.subtype === 'channel_join' || message.subtype === 'channel_leave') {
+			await slack.chat.delete({
+				token: process.env.HAKATASHI_TOKEN,
+				channel: message.channel,
+				ts: message.ts,
+			});
 		}
 	});
 };
