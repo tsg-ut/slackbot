cf841c42e43592cce2a157932c315430e9bdadf5
Yosshi999 <Yosshi999@users.noreply.github.com>
2021-10-14 16:34:15 +0000

    apply suggestions

--- discord/speeches/voicevox.ts
+++ discord/speeches/voicevox.ts
@@ -8,26 +8,25 @@ enum VoiceType {
 	zundamon = 1,
 }
 
+const voiceMapping: { [name: string]: { [emo: string]: number } } = {
+	metan: {
+		normal: 0,
+		happiness: 2,
+		anger: 4,
+		sadness: 6,
+	},
+	zundamon: {
+		normal: 1,
+		happiness: 3,
+		anger: 5,
+		sadness: 7,
+	},
+};
+
 const speech: SynthesizeFunction = (text: string, voiceType: string, {speed, emotion}) => {
 	if (text.length >= 30) {
-		return new Promise((_, reject) => {
-			reject(new Error('Text must be shorter than 30 characters for VOICEVOX.'));
-		});
+		return Promise.reject(new Error('Text must be shorter than 30 characters for VOICEVOX.'));
 	}
-	const voiceMapping: { [name: string]: { [emo: string]: number } } = {
-		metan: {
-			normal: 0,
-			happiness: 2,
-			anger: 4,
-			sadness: 6,
-		},
-		zundamon: {
-			normal: 1,
-			happiness: 3,
-			anger: 5,
-			sadness: 7,
-		},
-	};
 	const postData = {
 		text,
 		speaker: voiceMapping[voiceType][emotion],
