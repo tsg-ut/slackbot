aad96ee86a0332f47467ff6ee70a8adbbf9ce366
cookie-s <kcz@kcz.sh>
2022-02-20 14:15:19 -0500

    more comment

--- lib/slackEventClient.ts
+++ lib/slackEventClient.ts
@@ -19,7 +19,8 @@ export class TSGEventClient {
 		return this.eventAdapter.on(event, (...args: any[]) => {
 			// https://slack.dev/node-slack-sdk/events-api#receive-additional-event-data
 			// https://github.com/slackapi/node-slack-sdk/blob/3e9c483c593d6aa28f6f5680f287722df3327609/packages/events-api/src/http-handler.ts#L212-L223
-			// args: [body.event, body]
+			// https://api.slack.com/apis/connections/events-api#the-events-api__receiving-events__events-dispatched-as-json
+			// args: [body.event, body: {team_id: string}]
 			if (args[1].team_id === this.team) {
 				listener(...args);
 			}
