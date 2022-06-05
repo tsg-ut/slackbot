e4da2996039fb75a5f9f0a1754e4086c38cf1ce2
tsg <info@tsg.jp>
2022-06-05 16:41:45 +0900

    Fix sqlite3 installation

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	apps : [{
 		name: 'app',
 		interpreter: 'node',
-		interpreter_args: '-r ts-node/register/transpile-only',
+		interpreter_args: '-r ts-node/register/transpile-only --max_old_space_size=8192',
 		script: 'index.ts',
 		watch: false,
 		min_uptime: '60s',
