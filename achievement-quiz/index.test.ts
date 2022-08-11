e88e23c10819cf3a479d3734998cd16ae26b23a9
settyan117 <sute-kidaisuki@hotmail.co.jp>
2022-08-11 18:33:08 +0900

    [update] ほんのちょっとテストを追加

--- achievement-quiz/index.test.ts
+++ achievement-quiz/index.test.ts
@@ -9,3 +9,11 @@ beforeEach(() => {
   process.env.CHANNEL_SANDBOX = slack.fakeChannel;
   achievementQuiz(slack);
 });
+
+describe('response to /^実績当てクイズ$/', () => {
+  it('starts game by "実績当てクイズ"', async () => {
+    const response = await slack.getResponseTo('実績当てクイズ');
+    expect(response.username).toBe('実績当てクイズ');
+    expect(response.text).toContain('この実績なーんだ');
+  });
+});
