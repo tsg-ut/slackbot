5a2763be9cfd0b5c0b8b908fb8881b4a0f62be1f
Koki Takahashi <hakatasiloving@gmail.com>
2025-06-21 10:34:10 +0900

    slow-quiz: Use slash command /slow-quiz to show current status of slow quiz

--- slow-quiz/index.ts
+++ slow-quiz/index.ts
@@ -1,9 +1,11 @@
 import {readFile} from 'fs/promises';
 import path from 'path';
 import {SlackMessageAdapter} from '@slack/interactive-messages';
-import type {ChatPostMessageArguments, ImageElement, KnownBlock, WebClient} from '@slack/web-api';
+import type {ImageElement, KnownBlock, WebClient} from '@slack/web-api';
 import {Mutex} from 'async-mutex';
 import {oneLine, stripIndent} from 'common-tags';
+import type {FastifyPluginCallback} from 'fastify';
+import plugin from 'fastify-plugin';
 // @ts-expect-error: Not typed
 import {hiraganize} from 'japanese';
 import yaml from 'js-yaml';
@@ -13,7 +15,7 @@ import type OpenAI from 'openai';
 import {increment} from '../achievements';
 import logger from '../lib/logger';
 import openai from '../lib/openai';
-import {eventClient, type SlackInterface} from '../lib/slack';
+import type {SlashCommandEndpoint, SlackInterface} from '../lib/slack';
 import State from '../lib/state';
 import {Loader} from '../lib/utils';
 import {getUserIcon, getUserMention, getUserName} from './util';
@@ -787,22 +789,31 @@ class SlowQuiz {
 
 		await this.checkGameEnd();
 		if (this.state.games.some((game) => game.status === 'inprogress')) {
-			await this.postGameStatus();
+			await this.postGameStatus(true);
 		}
 		await this.createBotAnswers();
 	}
 
-	async postGameStatus() {
+	async postGameStatus(replaceLatestStatusMessages: boolean, channels: string[] = []) {
 		const blocks = await this.getGameBlocks();
-		const messages = await this.postMessage({
-			text: '現在開催中の1日1文字クイズ一覧',
-			blocks,
-		});
+		const messages = await this.postMessage(
+			{
+				text: '現在開催中の1日1文字クイズ一覧',
+				blocks,
+			},
+			...(channels.length > 0 ? [channels] : []),
+		);
 
-		this.state.latestStatusMessages = messages.map((message) => ({
+		const newStatusMessages = messages.map((message) => ({
 			ts: message.ts,
 			channel: message.channel,
 		}));
+
+		if (replaceLatestStatusMessages) {
+			this.state.latestStatusMessages = newStatusMessages;
+		} else {
+			this.state.latestStatusMessages.push(...newStatusMessages);
+		}
 	}
 
 	chooseNewGame() {
@@ -1069,10 +1080,13 @@ class SlowQuiz {
 		return {text, invisibleCharacters};
 	}
 
-	async postMessage(message: {text: string, blocks: KnownBlock[]}) {
+	async postMessage(
+		message: {text: string, blocks: KnownBlock[]},
+		channels: string[] = [process.env.CHANNEL_SANDBOX, process.env.CHANNEL_QUIZ],
+	) {
 		const messages = [];
 
-		for (const channel of [process.env.CHANNEL_SANDBOX, process.env.CHANNEL_QUIZ]) {
+		for (const channel of channels) {
 			const response = await this.slack.chat.postMessage({
 				channel,
 				username: '1日1文字クイズ',
@@ -1107,22 +1121,36 @@ class SlowQuiz {
 	}
 }
 
-export default async ({webClient: slack, messageClient: slackInteractions}: SlackInterface) => {
-	const slowquiz = new SlowQuiz({slack, slackInteractions});
-	await slowquiz.initialize();
+export const server = ({webClient: slack, messageClient: slackInteractions}: SlackInterface) => {
+	const callback: FastifyPluginCallback = async (fastify, opts, next) => {
+		const slowquiz = new SlowQuiz({slack, slackInteractions});
+		await slowquiz.initialize();
 
-	eventClient.on('message', (message) => {
-		if (message.text === 'slow-quiz') {
+		fastify.post<SlashCommandEndpoint>('/slash/slow-quiz', (req, res) => {
+			if (req.body.token !== process.env.SLACK_VERIFICATION_TOKEN) {
+				res.code(400);
+				return 'Bad Request';
+			}
+
 			mutex.runExclusive(async () => {
 				log.info('Received /slow-quiz command');
-				await slowquiz.postGameStatus();
+				await slowquiz.postGameStatus(false, [req.body.channel_id]);
 			});
-		}
-	});
 
-	scheduleJob('0 10 * * *', () => {
-		mutex.runExclusive(() => {
-			slowquiz.progressGames();
+			return {
+				response_type: 'in_channel',
+				text: 'Working...',
+			};
 		});
-	});
+
+		scheduleJob('0 10 * * *', () => {
+			mutex.runExclusive(() => {
+				slowquiz.progressGames();
+			});
+		});
+
+		next();
+	};
+
+	return plugin(callback);
 };
