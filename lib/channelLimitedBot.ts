326bf926533946f628bd37d92876f6ba5baf6a0b
Koki Takahashi <hakatasiloving@gmail.com>
2025-12-22 11:51:04 +0000

    Implement progressMessage support in channelLimitedBot and support it in wordhero/crossword

--- lib/channelLimitedBot.ts
+++ lib/channelLimitedBot.ts
@@ -3,20 +3,28 @@ import {extractMessage, isGenericMessage} from './slackUtils';
 import type {GenericMessageEvent, WebClient} from '@slack/web-api';
 import type {MessageEvent} from '@slack/bolt';
 import logger from './logger';
+import { Deferred } from './utils';
 
 export class ChannelLimitedBot {
-	protected slack: WebClient;
-	protected eventClient: SlackInterface['eventClient'];
-	protected wakeWordRegex = /.*/;
-	protected allowedChannels: string[] = [process.env.CHANNEL_GAMES!];
+	protected readonly slack: WebClient;
+	protected readonly eventClient: SlackInterface['eventClient'];
+	protected readonly messageClient: SlackInterface['messageClient'];
 
-	protected log = logger.child({bot: this.constructor.name});
+	protected readonly log = logger.child({bot: this.constructor.name});
+
+	protected readonly wakeWordRegex = /.*/;
+	protected readonly allowedChannels: string[] = [process.env.CHANNEL_GAMES!];
+	protected readonly username: string = 'slackbot';
+	protected readonly iconEmoji: string = ':robot_face:';
+	protected readonly progressMessageChannel: string | undefined = process.env.CHANNEL_SANDBOX;
+	protected readonly progressMessages: Map<string, Deferred<string>> = new Map(); // gameMessageTs -> Deferred<progressMessageTs>
 
 	constructor(
 		slackClients: SlackInterface,
 	) {
 		this.slack = slackClients.webClient;
 		this.eventClient = slackClients.eventClient;
+		this.messageClient = slackClients.messageClient;
 
 		this.log.info(`Initialized ${this.constructor.name}`);
 		this.log.info(`Allowed channels: ${this.allowedChannels.join(', ')}`);
@@ -37,39 +45,113 @@ export class ChannelLimitedBot {
 			return;
 		}
 
-		if (this.wakeWordRegex.test(message.text)) {
-			const channel = this.allowedChannels.includes(message.channel) ? message.channel : this.allowedChannels[0];
+		if (!this.wakeWordRegex.test(message.text)) {
+			return;
+		}
 
-			const responseTs = await this.onWakeWord(message, channel);
+		const channel = this.allowedChannels.includes(message.channel) ? message.channel : this.allowedChannels[0];
 
-			if (!this.allowedChannels.includes(message.channel)) {
-				if (responseTs === null) {
+		const responseTs = await this.onWakeWord(message, channel);
+		const progressMessageDeferred = new Deferred<string>();
+		if (responseTs !== null) {
+			this.progressMessages.set(responseTs, progressMessageDeferred);
+		}
+
+		const isAllowedChannel = this.allowedChannels.includes(message.channel);
+
+		if (responseTs === null) {
+			if (!isAllowedChannel) {
+				await this.slack.chat.postEphemeral({
+					channel: message.channel,
+					user: message.user,
+					text: 'このチャンネルではBOTを実行できません。',
+				});
+			}
+		} else {
+			const {permalink: responseUrl} = await this.slack.chat.getPermalink({
+				channel,
+				message_ts: responseTs,
+			});
+
+			if (!responseUrl) {
+				this.log.error('Failed to get permalink for response message');
+			} else {
+				if (!isAllowedChannel) {
 					await this.slack.chat.postEphemeral({
 						channel: message.channel,
 						user: message.user,
-						text: 'このチャンネルではBOTを実行できません。',
-					});
-				} else {
-					const responseUrl = await this.slack.chat.getPermalink({
-						channel,
-						message_ts: responseTs,
-					});
-					await this.slack.chat.postEphemeral({
-						channel: message.channel,
-						user: message.user,
-						text: `このチャンネルではBOTを実行できません。代わりに<${responseUrl.permalink}|こちら>で実行しました。`,
+						text: `このチャンネルではBOTを実行できません。代わりに<${responseUrl}|こちら>で実行しました。`,
 					});
 				}
 
-				await this.deleteMessage(event);
+				const progressMessageTs = await this.postProgressMessage(channel, responseTs, responseUrl);
+				if (progressMessageTs) {
+					progressMessageDeferred.resolve(progressMessageTs);
+				} else {
+					progressMessageDeferred.reject(new Error('Failed to post progress message'));
+				}
 			}
+		}
+
+		if (!isAllowedChannel) {
+			await this.slack.chat.delete({
+				token: process.env.HAKATASHI_TOKEN,
+				channel: event.channel,
+				ts: event.ts!,
+			});
 		}
 	}
 
-	protected deleteMessage(event: MessageEvent) {
-		return this.slack.chat.delete({
-			channel: event.channel,
-			ts: event.ts!,
+	protected async postProgressMessage(
+		gameMessageChannel: string,
+		gameMessageTs: string,
+		gameMessageUrl?: string,
+	): Promise<string | undefined> {
+		if (
+			this.progressMessageChannel === undefined ||
+			this.progressMessageChannel === gameMessageChannel
+		) {
+			return undefined;
+		}
+
+		const gameMessageLink = gameMessageUrl ?? (await this.slack.chat.getPermalink({
+			channel: gameMessageChannel,
+			message_ts: gameMessageTs,
+		})).permalink;
+
+		if (gameMessageLink === undefined) {
+			this.log.error('Failed to get permalink for game message');
+			return undefined;
+		}
+
+		const progressMessage = await this.slack.chat.postMessage({
+			channel: this.progressMessageChannel,
+			text: `<${gameMessageLink}|進行中のゲーム>があります！`,
+			username: this.username,
+			icon_emoji: this.iconEmoji,
+			unfurl_links: false,
+			unfurl_media: false,
+		});
+
+		return progressMessage.ts;
+	}
+
+	protected async deleteProgressMessage(gameMessageTs: string) {
+		if (this.progressMessageChannel === undefined) {
+			return;
+		}
+
+		const progressMessageDeferred = this.progressMessages.get(gameMessageTs);
+		if (progressMessageDeferred === undefined) {
+			return;
+		}
+
+		this.progressMessages.delete(gameMessageTs);
+		const progressMessageTs = await progressMessageDeferred.promise;
+
+		await this.slack.chat.delete({
+			channel: this.progressMessageChannel,
+			ts: progressMessageTs,
 		});
 	}
 
