72201256d7bc0b29a14eec2447c4a17c1d8a3d3c
Koki Takahashi <hakatasiloving@gmail.com>
2026-03-02 20:30:13 +0000

    fix(nmpz): specify type for data in getCountryName function

--- nmpz/index.ts
+++ nmpz/index.ts
@@ -72,7 +72,7 @@ const getCountryName = async (country_code: string): Promise<Country> => new Pro
   const url = "https://raw.githubusercontent.com/mledoze/countries/master/countries.json";
   fetch(url)
     .then((response) => response.json())
-    .then((data) => {
+    .then((data: any[]) => {
       const country = data.find((c: any) => c.cca2 === country_code.toUpperCase());
       if (!country) {
         reject(new Error("Country not found"));
